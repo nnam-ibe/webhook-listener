60129b20e0bdd65228f6b96b6d13cefc625c3cd2
Nnamdi Ibe <nnam.ibe@gmail.com>
2024-05-30 00:13:45 -0400

    add update and rebuild functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 #[macro_use]
 extern crate rocket;
+use dotenv::dotenv;
+use repo::{get_repo, rebuild_container, update_repo};
 use rocket::serde::{json::Json, Deserialize};
+mod repo;
+
+#[derive(Deserialize, PartialEq, Eq)]
+#[serde(crate = "rocket::serde")]
+struct Repository {
+    full_name: String,
+}
 
 #[derive(Deserialize, PartialEq, Eq)]
 #[serde(crate = "rocket::serde")]
@@ -13,6 +22,7 @@ struct PullRequest {
 struct Event<'r> {
     action: &'r str,
     pull_request: PullRequest,
+    repository: Repository,
 }
 
 #[get("/")]
@@ -35,10 +45,32 @@ fn hook(event: Json<Event<'_>>) -> &str {
     }
 
     println!("A merged event!");
-    event.action
+    let repo = match get_repo(event.repository.full_name) {
+        Some(i) => i,
+        None => {
+            panic!("Unknown repo!");
+        }
+    };
+
+    update_repo(&repo);
+    rebuild_container(&repo);
+
+    "Done"
 }
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().ok();
+
+    let repos = std::env::var("REPOS").expect("REPOS must be set");
+    let dirs = std::env::var("DIRS").expect("DIRS must be set");
+
+    let repos_vector: Vec<String> = repos.split(',').map(|s| s.to_string()).collect();
+    let dirs_vector: Vec<String> = dirs.split(',').map(|s| s.to_string()).collect();
+
+    if repos_vector.len() != dirs_vector.len() {
+        panic!("Env vars must be of the same length");
+    }
+
     rocket::build().mount("/", routes![index, hook])
 }
---
+++ src/repo.rs
@@ -0,0 +1,69 @@
+use dotenv::dotenv;
+use std::process::Command;
+
+pub struct Repo {
+    pub name: String,
+    pub path: String,
+}
+
+//TODO: repos_vector & dirs_vector should be passed in as an argument
+pub fn get_repo(name: String) -> Option<Repo> {
+    dotenv().ok();
+
+    let repos = std::env::var("REPOS").expect("REPOS must be set");
+    let dirs = std::env::var("DIRS").expect("DIRS must be set");
+
+    let repos_vector: Vec<String> = repos.split(',').map(|s| s.to_string()).collect();
+    let dirs_vector: Vec<String> = dirs.split(',').map(|s| s.to_string()).collect();
+
+    let index = repos_vector.iter().position(|r| r == &name);
+    match index {
+        Some(i) => {
+            let path = dirs_vector.get(i);
+            path.map(|p| Repo {
+                name,
+                path: p.clone(),
+            })
+        }
+        None => {
+            println!("Unknown repo");
+            None
+        }
+    }
+}
+
+pub fn update_repo(repo: &Repo) {
+    let mut command = Command::new("git");
+    command.arg("pull");
+    command.current_dir(repo.path.clone());
+
+    let out = command.output().expect("Command failed");
+    let data = match out.status.success() {
+        true => out.stdout,
+        false => out.stderr,
+    };
+
+    let s = match String::from_utf8(data) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    println!("{}", s);
+}
+
+pub fn rebuild_container(repo: &Repo) {
+    let mut command = Command::new("docker-compose");
+    command.args(["up", "-d", "--build"]);
+    command.current_dir(repo.path.clone());
+
+    let out = command.output().expect("Command failed");
+    let data = match out.status.success() {
+        true => out.stdout,
+        false => out.stderr,
+    };
+
+    let s = match String::from_utf8(data) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    println!("{}", s);
+}
